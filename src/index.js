51221882358a908966e687ca4f19e9c35f20c137
Robert Klebe <robert.klebe@neovici.se>
2020-01-09 13:13:49 +0100

    feat: export relativetime

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 /* @flow strict */
 
 import LocalTimeElement from './local-time-element'
+import RelativeTime from './relative-time'
 import RelativeTimeElement from './relative-time-element'
 import TimeAgoElement from './time-ago-element'
 import TimeUntilElement from './time-until-element'
 
-export {LocalTimeElement, RelativeTimeElement, TimeAgoElement, TimeUntilElement}
+export {LocalTimeElement, RelativeTime, RelativeTimeElement, TimeAgoElement, TimeUntilElement}
